1e54065ffc5eb559fb3052530ab81b9e9e95821c
zulqii777 <80761391+zulqii777@users.noreply.github.com>
2021-11-11 04:41:14 -0800

    project

---
+++ resources/views/layout/loginheader.blade.php
@@ -0,0 +1,49 @@
+<header class="showcase">
+
+
+        <div class="logo">
+            <img src="https://i.ibb.co/r5krrdz/logo.png">
+        </div>
+
+        <div class="showcase-content">
+            <div class="formm">
+                <form >
+                    <h1>Sign In</h1>
+                    <div class="info">
+                        <input class="email" type="email" placeholder="Email or phone number"> <br>
+                        <input class="email" type="password" placeholder="Password">
+                    </div>
+                    <div class="btn">
+                        <button class="btn-primary" type="submit">Sign In</button>
+                    </div>
+                    <div class="help">
+                        <div>
+                            <input value="true" type="checkbox"><label>Remember me</label>
+                        </div>
+
+                        <a href="https://www.netflix.com/dz-en/LoginHelp">Need Help ?</a>
+
+                    </div>
+
+                </form>
+
+            </div>
+
+            
+            <div class="signup">
+                <p>New to Netflix ? <a href="pages/appsignup">Sign up now</a> </p>
+                
+            </div>
+            <div class="more">
+                <p>
+                    This page is protected by Google reCAPTCHA to ensure you're not a bot. <a href="#">Learn more.</a>
+                </p>
+            </div>
+
+
+        </div>
+
+
+
+
+    </header>
---
+++ resources/views/layout/signupheader.blade.php
@@ -0,0 +1,50 @@
+<header class="showcase">
+
+
+<div class="logo">
+    <img src="https://i.ibb.co/r5krrdz/logo.png">
+</div>
+
+<div class="showcase-content">
+    <div class="formm">
+        <form action="signup" method="post">
+            <h1>Sign Up</h1>
+            <div class="info">
+                <input class="email" type="name" placeholder="Username"> <br>
+                <input class="email" type="email" placeholder="Email or phone number"> <br>
+                <input class="email" type="password" placeholder="Password">
+            </div>
+            <div class="btn">
+                <button class="btn-primary" type="submit">Sign up</button>
+            </div>
+            <div class="help">
+                <div>
+                    <input value="true" type="checkbox"><label>Remember me</label>
+                </div>
+
+
+
+            </div>
+
+        </form>
+
+    </div>
+
+
+    <div class="signup">
+        <p>Already have account ?</p>
+        <a href="pages/applogin">Sign In now</a>
+    </div>
+    <div class="more">
+        <p>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. <a href="#">Learn more.</a>
+        </p>
+    </div>
+
+
+</div>
+
+
+
+
+</header>
